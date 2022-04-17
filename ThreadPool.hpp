939d1c5791c6a7cd10ee72763d5d4597632cccda
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-04-18 03:07:19 +0800

    format

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -22,40 +22,40 @@ class thread_pool
 public:
 	explicit thread_pool(std::size_t thread_count = std::thread::hardware_concurrency())
 	{
-		for (std::size_t i{ 0 }; i < thread_count; ++i) {
+		for (std::size_t i{0}; i < thread_count; ++i)
+		{
 			m_workers.emplace_back(std::bind(&thread_pool::thread_loop, this));
 		}
 	}
 
 	~thread_pool()
 	{
-		if (m_workers.size() > 0) {
+		if (m_workers.size() > 0)
+		{
 			join();
 		}
 	}
 
-	thread_pool(thread_pool const&) = delete;
-	thread_pool(thread_pool&&) = default;
+	thread_pool(thread_pool const &) = delete;
+	thread_pool(thread_pool &&) = default;
 
-	thread_pool& operator=(thread_pool const&) = delete;
-	thread_pool& operator=(thread_pool&&) = default;
+	thread_pool &operator=(thread_pool const &) = delete;
+	thread_pool &operator=(thread_pool &&) = default;
 
 	// Push a new task into the queue
 	template <class Func, class... Args>
-	auto push(Func&& fn, Args &&...args)
+	auto push(Func &&fn, Args &&...args)
 	{
 		using return_type = typename std::result_of<Func(Args...)>::type;
 
-		auto task{ std::make_shared<std::packaged_task<return_type()>>(
-			std::bind(std::forward<Func>(fn), std::forward<Args>(args)...)
-		) };
+		auto task{std::make_shared<std::packaged_task<return_type()>>(
+			std::bind(std::forward<Func>(fn), std::forward<Args>(args)...))};
 
-		auto future{ task->get_future() };
-		std::unique_lock<std::mutex> lock{ m_mutex };
+		auto future{task->get_future()};
+		std::unique_lock<std::mutex> lock{m_mutex};
 
-		m_tasks.emplace([task]() {
-			(*task)();
-			});
+		m_tasks.emplace([task]()
+						{ (*task)(); });
 
 		lock.unlock();
 		m_notifier.notify_one();
@@ -65,9 +65,10 @@ public:
 	// Remove all pending tasks from the queue
 	void clear()
 	{
-		std::unique_lock<std::mutex> lock{ m_mutex };
+		std::unique_lock<std::mutex> lock{m_mutex};
 
-		while (!m_tasks.empty()) {
+		while (!m_tasks.empty())
+		{
 			m_tasks.pop();
 		}
 	}
@@ -78,8 +79,10 @@ public:
 		m_stop = true;
 		m_notifier.notify_all();
 
-		for (auto& thread : m_workers) {
-			if (thread.joinable()) {
+		for (auto &thread : m_workers)
+		{
+			if (thread.joinable())
+			{
 				thread.join();
 			}
 		}
@@ -103,16 +106,19 @@ private:
 	// Thread main loop
 	void thread_loop()
 	{
-		while (true) {
+		while (true)
+		{
 			// Wait for a new task
-			auto task{ next_task() };
+			auto task{next_task()};
 
-			if (task) {
+			if (task)
+			{
 				++m_active;
 				task();
 				--m_active;
 			}
-			else if (m_stop) {
+			else if (m_stop)
+			{
 				// No more task + stop required
 				break;
 			}
@@ -122,24 +128,24 @@ private:
 	// Get the next pending task
 	task_type next_task()
 	{
-		std::unique_lock<std::mutex> lock{ m_mutex };
+		std::unique_lock<std::mutex> lock{m_mutex};
 
-		m_notifier.wait(lock, [this]() {
-			return !m_tasks.empty() || m_stop;
-			});
+		m_notifier.wait(lock, [this]()
+						{ return !m_tasks.empty() || m_stop; });
 
-		if (m_tasks.empty()) {
+		if (m_tasks.empty())
+		{
 			// No pending task
 			return {};
 		}
 
-		auto task{ m_tasks.front() };
+		auto task{m_tasks.front()};
 		m_tasks.pop();
 		return task;
 	}
 
-	std::atomic<bool> m_stop{ false };
-	std::atomic<std::size_t> m_active{ 0 };
+	std::atomic<bool> m_stop{false};
+	std::atomic<std::size_t> m_active{0};
 
 	std::condition_variable m_notifier;
 	std::mutex m_mutex;
--- sum-time-test.cpp
+++ sum-time-test.cpp
@@ -1,3 +1,8 @@
+/*
+ Copyright (c) 2022 Howard Wu
+ Licensed under the MIT Licence
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <thread>
@@ -9,7 +14,8 @@ constexpr auto MAX_NUM = 6000000000;
 
 int unsigned_int_overflow(unsigned int var_a, unsigned int var_b)
 {
-	if (UINT_MAX - var_a < var_b) {
+	if (UINT_MAX - var_a < var_b)
+	{
 		std::cout << "warn: unsigned_int_overflow!" << std::endl;
 		return 1;
 	}
@@ -18,7 +24,8 @@ int unsigned_int_overflow(unsigned int var_a, unsigned int var_b)
 }
 int unsigned_llong_overflow(unsigned long long var_a, unsigned long long var_b)
 {
-	if (ULLONG_MAX - var_a < var_b) {
+	if (ULLONG_MAX - var_a < var_b)
+	{
 		std::cout << "warn: unsigned_llong_overflow!" << std::endl;
 		return 1;
 	}
@@ -26,12 +33,15 @@ int unsigned_llong_overflow(unsigned long long var_a, unsigned long long var_b)
 		return 0;
 }
 
-void sumUpTasks(std::promise<unsigned long long>&& prom, const unsigned long long start, const unsigned long long end) {
+void sumUpTasks(std::promise<unsigned long long> &&prom, const unsigned long long start, const unsigned long long end)
+{
 	unsigned long long sum = {};
 	auto temp = end;
-	do {
+	do
+	{
 #ifdef _DEBUG
-		if (unsigned_llong_overflow(sum, temp) == 1) return;
+		if (unsigned_llong_overflow(sum, temp) == 1)
+			return;
 #endif // _DEBUG
 		sum += temp;
 		temp--;
@@ -39,18 +49,21 @@ void sumUpTasks(std::promise<unsigned long long>&& prom, const unsigned long lon
 	prom.set_value(sum);
 }
 thread_local unsigned long long tmpSum = 0;
-void sumUp(std::atomic<unsigned long long>& sum, const unsigned long long start, const unsigned long long end) {
+void sumUp(std::atomic<unsigned long long> &sum, const unsigned long long start, const unsigned long long end)
+{
 	auto temp = end;
-	do {
+	do
+	{
 #ifdef _DEBUG
-		if (unsigned_llong_overflow(tmpSum, temp) == 1)return;
+		if (unsigned_llong_overflow(tmpSum, temp) == 1)
+			return;
 #endif // _DEBUG
 		tmpSum += temp;
 		temp--;
 	} while (temp >= start);
 	sum.fetch_add(tmpSum, std::memory_order_relaxed);
 }
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	unsigned long long count_to = MAX_NUM;
 	unsigned short thread_count = 2;
@@ -58,12 +71,14 @@ int main(int argc, char* argv[])
 	std::cout << "Time Test of Single-threaded and Multi-threaded summation" << std::endl;
 
 #ifdef _DEBUG
-	std::cout << "DEBUG builds may cause performance degradation!" << std::endl << std::endl;
+	std::cout << "DEBUG builds may cause performance degradation!" << std::endl
+			  << std::endl;
 #endif // _DEBUG
 
 	std::cout << std::setiosflags(std::ios::fixed);
 
-	if (argc >= 3) {
+	if (argc >= 3)
+	{
 		thread_count = atoi(argv[1]);
 		auto input_num = strtoull(argv[2], NULL, 10);
 		if (input_num <= MAX_NUM)
@@ -75,17 +90,19 @@ int main(int argc, char* argv[])
 
 	auto start_time = std::chrono::steady_clock::now();
 
-	//std::promise<unsigned long long> prom1;
-	//auto fut1 = prom1.get_future();
-	//std::thread t1(sumUp, std::move(prom1), 1, count_to);
-	//sumUpReturn(std::ref(sum), 1, count_to);
-	//sum = fut1.get();
+	// std::promise<unsigned long long> prom1;
+	// auto fut1 = prom1.get_future();
+	// std::thread t1(sumUp, std::move(prom1), 1, count_to);
+	// sumUpReturn(std::ref(sum), 1, count_to);
+	// sum = fut1.get();
 
 	std::thread t1(sumUp, std::ref(sum), 1, count_to);
 	t1.join();
 
 	auto diff = std::chrono::steady_clock::now() - start_time;
-	std::cout << "Single-threaded:" << std::endl << "time=" << std::chrono::duration <double, std::milli>(diff).count() << "ms" << std::endl << "sum=" << sum << std::endl;
+	std::cout << "Single-threaded:" << std::endl
+			  << "time=" << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl
+			  << "sum=" << sum << std::endl;
 	// End Single-threaded
 
 	// Multi-threaded
@@ -93,16 +110,20 @@ int main(int argc, char* argv[])
 	unsigned long long start;
 	unsigned long long end;
 	auto block_size = count_to / thread_count;
-	thread_pool pool{ thread_count };
+	thread_pool pool{thread_count};
 
 	start_time = std::chrono::steady_clock::now();
-	for (auto i = 0; i < thread_count; i++) {
+	for (auto i = 0; i < thread_count; i++)
+	{
 		start = i * block_size + 1;
 		end = (static_cast<unsigned long long>(i) + 1) * block_size;
 		pool.push(std::bind(sumUp, std::ref(sum), start, end));
 	}
 	pool.join();
 	diff = std::chrono::steady_clock::now() - start_time;
-	std::cout << "Multi-threaded:" << std::endl << "threads=" << thread_count << std::endl << "time=" << std::chrono::duration <double, std::milli>(diff).count() << "ms" << std::endl << std::setprecision(0) << "sum=" << sum;
+	std::cout << "Multi-threaded:" << std::endl
+			  << "threads=" << thread_count << std::endl
+			  << "time=" << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl
+			  << std::setprecision(0) << "sum=" << sum;
 	// End Multi-threaded
 }
